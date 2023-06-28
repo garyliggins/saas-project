b3fda9b93e2fb3099ee65b24994adc82a1902370
gary <gary@ligginsdevelopment.com>
2023-06-27 23:12:00 -0700

    starting to connect to products data

---
+++ src/core/components/Meta.js
@@ -0,0 +1,11 @@
+export default function Meta() {
+  return (
+    <>
+      <title>Epic products</title>
+      <meta
+        name="description"
+        content="All of the most epic products in one place."
+      />
+    </>
+  );
+}
